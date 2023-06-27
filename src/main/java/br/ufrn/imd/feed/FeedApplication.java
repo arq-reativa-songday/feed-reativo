1ccc5cd008fa4e6faeab8206144d97ae16521827
Márcio Tenório <marciotenorioj@gmail.com>
2023-06-27 15:03:04 -0300

    register in discovery and have route in gateway

--- src/main/java/br/ufrn/imd/feed/FeedApplication.java
+++ src/main/java/br/ufrn/imd/feed/FeedApplication.java
@@ -2,12 +2,14 @@ package br.ufrn.imd.feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
 @EnableFeignClients
+@EnableDiscoveryClient
 public class FeedApplication {
 
 	public static void main(String[] args) {
