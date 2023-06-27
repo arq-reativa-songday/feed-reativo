2087292bd538473791b5ea4d2441198baf22f7ce
mariaeloi <mariaeduardaeloi@outlook.com>
2023-06-26 22:54:48 -0300

    refac: remove dependencia desnecessaria

--- src/main/java/br/ufrn/imd/feed/FeedApplication.java
+++ src/main/java/br/ufrn/imd/feed/FeedApplication.java
@@ -2,12 +2,10 @@ package br.ufrn.imd.feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
-@EnableFeignClients
 public class FeedApplication {
 
 	public static void main(String[] args) {
